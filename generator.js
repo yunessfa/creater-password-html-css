b8510b5d08201048bbbb80a5e15adfe4883ce8fd
yunessfa <107870249+yunessfa@users.noreply.github.com>
2022-12-25 13:44:42 +0330

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,23 @@
+const display = document.querySelector("input"),
+    button = document.querySelector("button"),
+    copyBtn = document.querySelector("span.far"),
+    copyActive = document.querySelector("span.fas");
+const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+button.onclick = () => {
+    let i,
+        randomPassword = "";
+    copyBtn.style.display = "block";
+    copyActive.style.display = "none";
+    for (i = 0; i < 10; i++) {
+        randomPassword =
+            randomPassword + chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    display.value = randomPassword;
+};
+function copy() {
+    copyBtn.style.display = "none";
+    copyActive.style.display = "block";
+    display.select();
+    document.execCommand("copy");
+}
